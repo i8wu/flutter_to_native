0874dfa5a9bd03f33840174e86abf5ae1fccfe45
Ivan Wu <ivanchungwu@gmail.com>
2020-07-31 06:26:18 -0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_to_native/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.flutter_to_native
+
+import android.os.Bundle
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "flutter.native/helper"
+    private val HELLO_METHOD = "helloFromNativeCode"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if (call.method == HELLO_METHOD) {
+                val greetings = helloFromNativeCode()
+                result.success(greetings)
+            }
+            else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    fun helloFromNativeCode(): String {
+        return "Hello from native android"
+    }
+}
