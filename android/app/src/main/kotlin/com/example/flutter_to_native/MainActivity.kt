84f58d83c8a62277a55b60e36f374fb679519d14
Ivan Wu <ivanchungwu@gmail.com>
2020-08-03 11:16:07 -0400

    Added nearby sdk

--- android/app/src/main/kotlin/com/example/flutter_to_native/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_to_native/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.flutter_to_native
 
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import androidx.annotation.NonNull
+import com.google.android.gms.nearby.Nearby
+import com.google.android.gms.nearby.messages.Message
+import com.google.android.gms.nearby.messages.MessageListener
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
@@ -9,6 +14,38 @@ import io.flutter.plugin.common.MethodChannel
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "flutter.native/helper"
     private val HELLO_METHOD = "helloFromNativeCode"
+    private val TAG = "nearbyTagIvan"
+
+    private lateinit var mMessageListener : MessageListener
+    private lateinit var mMessage : Message
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mMessageListener = object : MessageListener() {
+            override fun onFound(message : Message) {
+                Log.d(TAG, "Found message: " + String(message.content));
+            }
+
+            override fun onLost(message : Message) {
+                Log.d(TAG, "Lost sight of message: " + String(message.content));
+            }
+        }
+
+        mMessage = Message("Hello World".toByteArray());
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Nearby.getMessagesClient(this).publish(mMessage)
+        Nearby.getMessagesClient(this).subscribe(mMessageListener)
+    }
+
+    override fun onStop() {
+        Nearby.getMessagesClient(this).unpublish(mMessage);
+        Nearby.getMessagesClient(this).unsubscribe(mMessageListener);
+        super.onStop();
+    }
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
